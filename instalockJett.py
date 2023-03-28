0eb2268173a0b6e006a9d2c008595e5e7cd2dce7
Schwitz <schwitz6969@gmail.com>
2023-03-28 14:58:07 +0530

    Added event to start the script and to end it

--- instalockJett.py
+++ instalockJett.py
@@ -1,6 +1,6 @@
 import pyautogui
 import time
-
+import keyboard
 print("Starting in 5 seconds...")
 print(pyautogui.size())
 print(pyautogui.position())
@@ -10,17 +10,21 @@ razeCordsX = 775
 razeCordsY = 984
 reynaCordsX = 883
 reynaCordsY = 997
-jettCordsX = 1090
+jettCordsX = 1050
 jettCordsY = 929
 
 
-
-
-def instalockAnything (x,y):
-    for i in range(0,51):
-        pyautogui.click(x,y)
-        pyautogui.click(1002,820)
-        clicked = i+1
-        print("Clicked " + str(clicked) + " times")
-
-instalockAnything(razeCordsX,razeCordsY) 
+while keyboard.is_pressed('e') == False:
+    time.sleep(0.1)
+    print("Waiting for F to be pressed")
+    if keyboard.is_pressed('f'):
+        print("F was pressed")
+        for i in range(0,51):
+            pyautogui.click(jettCordsX,jettCordsY)
+            pyautogui.click(1002,820)
+            clicked = i+1
+            print("Clicked " + str(clicked) + " times")
+            time.sleep(0.1)
+            if keyboard.is_pressed('e'):
+                print("E was pressed, exiting script")
+                break
