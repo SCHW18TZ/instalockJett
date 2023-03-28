735b3e8589e90bb2ed741e52ad3dd25d9f62f757
Schwitz <schwitz6969@gmail.com>
2023-03-28 14:25:13 +0530

    Change Func

--- instalockJett.py
+++ instalockJett.py
@@ -16,11 +16,11 @@ jettCordsY = 929
 
 
 
-# def instalockAnything (x,y):
-#     for i in range(0,51):
-#         pyautogui.click(x,y)
-#         pyautogui.click(1002,820)
-#         clicked = i+1
-#         print("Clicked " + str(clicked) + " times")
+def instalockAnything (x,y):
+    for i in range(0,51):
+        pyautogui.click(x,y)
+        pyautogui.click(1002,820)
+        clicked = i+1
+        print("Clicked " + str(clicked) + " times")
 
-# instalockAnything(razeCordsX,razeCordsY) 
+instalockAnything(razeCordsX,razeCordsY) 
