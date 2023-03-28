844ccfa5e3bc185fd778aed855f2d6a24e78aa2c
Schwitz <schwitz6969@gmail.com>
2023-03-28 14:20:39 +0530

    Added raze,reyna,jett cords x,y

--- instalockJett.py
+++ instalockJett.py
@@ -5,38 +5,21 @@ print("Starting in 5 seconds...")
 print(pyautogui.size())
 print(pyautogui.position())
 
-jettCords = (1090,929)
-reynaCords = (883,997)
 razeCordsX = 775
 razeCordsY = 984
-
-def intsalockJett():
-    for i in range(0,51):
-        pyautogui.click(1090,929)
-        pyautogui.click(1002,820)
-        clicked = i+1
-        print("Clicked " + str(clicked) + " times")   
-
-def instalockReyna():
-    for i in range(0,51):
-        pyautogui.click(883,997)
-        pyautogui.click(1002,820)
-        clicked = i+1
-        print("Clicked " + str(clicked) + " times")
-
-def instalockRaze():
-    for i in range(0,51):
-        pyautogui.click(775,984)
-        pyautogui.click(1002,820)
-        clicked = i+1
-        print("Clicked " + str(clicked) + " times")
+reynaCordsX = 883
+reynaCordsY = 997
+jettCordsX = 1090
+jettCordsY = 929
 
 
-def instalockAnything (x,y):
-    for i in range(0,51):
-        pyautogui.click(x,y)
-        pyautogui.click(1002,820)
-        clicked = i+1
-        print("Clicked " + str(clicked) + " times")
 
-instalockAnything(razeCordsX,razeCordsY) 
+
+# def instalockAnything (x,y):
+#     for i in range(0,51):
+#         pyautogui.click(x,y)
+#         pyautogui.click(1002,820)
+#         clicked = i+1
+#         print("Clicked " + str(clicked) + " times")
+
+# instalockAnything(razeCordsX,razeCordsY) 
